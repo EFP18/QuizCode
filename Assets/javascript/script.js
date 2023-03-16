5b9ddb7286e81f69d87ee8b5963f3ab33dd8009a
EFP18 <ester.p18@hotmail.com>
2023-03-15 22:09:15 -0500

    edited styles in css

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -3,12 +3,17 @@ var startPage = document.querySelector(".start-page");
 var questionDisplay = document.querySelector("#question");
 var quizButtonContainer = document.querySelector("#quiz-buttons");
 var finishedP = document.querySelector("#finished");
-
+var submitBtn = document.querySelector("#submit-button");
 var timer = document.querySelector(".timer");
+var userNotePTag = document.querySelector("#user-note");
 var timeLeft = 20;
 var timeStart;
+var highScores = document.querySelector("#highscores");
 
 var quizButtons = quizButtonContainer.querySelectorAll("button");
+
+var counter = 0;
+
 
 // var questionsContainer = document.querySelector(".questions-container") 
 
@@ -81,6 +86,7 @@ function startQuiz() {
 
 }
 
+
 var displayAnswer = function (){
   questionDisplay.textContent = questions[questionIndex].Question;
   for (var i=0; i<quizButtons.length; i++) {
@@ -88,26 +94,60 @@ var displayAnswer = function (){
   }
 }
 
-// for (var i=0; i < questionDisplay.length; i++){
-//   for (var j = 0 ; j < PossibleAnswers.length; j++) {
-//     if (PossibleAnswers[j] = CorrectAnswer){
-//       print = ("Correct!");
-//       console.log("correct");
-//     } else {
-//       print = ("Wrong!")
-//       console.log("wrong");
-    
+
+
+
+// function checkAnswer (){
+  
+  // var rightAnswer;
+  // var wrongAnswer;
+
+//   for (var i=0; i < questionDisplay.length; i++){
+//     for (var j = 0 ; j < PossibleAnswers.length; j++) {
+//       if (questions.PossibleAnswers[j] === questions.CorrectAnswer){
+//         print = ("Correct!");
+//         console.log("correct");
+//       } else {
+//         print = ("Wrong!")
+//         console.log("wrong");
+      
+//       }
 //     }
 //   }
 // }
 
+// buttonIndex = 0;
+
+// quizButtons.addEventListener("click", checkAnswer);
+
+
+startBtn.addEventListener("click", startQuiz);
+
+
 for (var i=0; i < quizButtons.length; i++){
-  quizButtons[i].addEventListener("click", function(){
+  quizButtons[i].addEventListener("click", function(event){
+    
+    var targetButtonIndex = parseInt(event.target.dataset.index);
+    
+    if (targetButtonIndex === questions[questionIndex].CorrectAnswer) {
+      console.log('correct');
+      
+      userNotePTag.textContent = 'Correct!'
+      // quizButtons.style.background = "green";
+
+    } else {
+      console.log('incorrect');
+      timeLeft = timeLeft - 10;
+      userNotePTag.textContent = 'Incorrect!'
+    }
+    
     questionIndex++;
-    if (questionIndex< questions.length){
+    
+    if (questionIndex < questions.length){
       displayAnswer();
     } else {
       questionDisplay.hidden=true;
+      userNotePTag.remove();
       quizButtonContainer.remove();
       endQuiz();
     }
@@ -119,17 +159,38 @@ function endQuiz() {
   clearInterval(timeStart);
   console.log('quiz ended')
   finishedP.hidden = false;
+  submitBtn.hidden = false;
+  highScores.hidden = true;
   questionDisplay.hidden = true;
   quizButtonContainer.remove();
+  userNotePTag.remove();
+
+
 }
 
+function submit(){
+  highScores.hidden = false;
+  finishedP.hidden=true;
+  submitBtn.hidden=true;
+  userNotePTag.remove();
 
-// timer interval
 
+}
 
+submitBtn.addEventListener("click", submit);
+
+// Show results and submit buttons
 
 
 // connect correct answer
 // print wrong or right
 // highscores
 // subtract time from clock for wrong answer
+
+// add event listener when hit the correct answer text to turn green 
+
+
+
+// local storage
+
+// submit even prevent default
