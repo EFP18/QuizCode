8349bc10e4a9791245ddf80c96cabfbeaca4f4fb
EFP18 <ester.p18@hotmail.com>
2023-03-18 19:54:32 -0500

    added right or wrong feedback

---
+++ Assets/javascript/script-new.js
@@ -0,0 +1,159 @@
+var mainEl = document.querySelector("main");
+var startPage = document.querySelector(".start-page");
+var startBtn = document.querySelector(".start-button");
+var questionsEl = document.querySelector(".questions");
+var quizDone = document.querySelector(".done");
+var scoreEl = document.querySelector(".score");
+var formInitials = document.querySelector(".form-initials");
+var submitBtn = document.querySelector(".submit-button");
+var highscoresEl = document.querySelector(".highscores");
+var timeLeft = document.querySelector(".time-left");
+
+var time = 50;
+var timerInterval;
+var questionIndex = 0;
+// start at 0 so it defaults by starting at the 1st question of the array of questions
+var rightOrWrong = "";
+
+
+
+var questionsObj = [
+  {
+  questionText: "What is the capital of Greece?", 
+  possibleAnswers: ["Athens", "Thessaloniki", "Patra"],
+  correctAnswer: 0
+  },
+
+  {
+  questionText: "What is a neighboring country of Greece?", 
+  possibleAnswers: ["Spain", "Switzerland", "Turkey"],
+  correctAnswer: 2
+  },
+
+  {
+  questionText: "What is the biggest port of Greece?", 
+  possibleAnswers: ["Patra", "Pireaus", "Thessaloniki"],
+  correctAnswer: 1
+  },
+
+  {
+  questionText: "What is a traditional food of Greece?", 
+  possibleAnswers: ["Souvlaki", "Pasta", "Falafel"],
+  correctAnswer: 0
+  },
+  
+  {
+  questionText: "Which city is the Acropolis in?", 
+  possibleAnswers: ["Athens", "Volos", "Kalamata"],
+  correctAnswer: 0
+  },
+]
+
+
+function startQuiz(){
+  mainEl.innerHTML = "";
+  // hide and show the things I want after the click of the start button
+
+  console.log("quiz started");
+
+
+  var timerInterval = setInterval(function(){
+    time--;
+    timeLeft.textContent = `Time left: ${time}` ;
+    
+    if (time <= 0){
+      clearInterval(timerInterval);
+      // print time is up score=0
+      endGame();
+      return;
+    } 
+    // stop the timer if it reaches 0
+    
+    // if all questions answered, clear interval
+
+  }, 1000);
+
+  displayQuestions();
+
+}
+
+startBtn.addEventListener("click", startQuiz);
+
+
+
+function displayQuestions(){
+  mainEl.innerHTML = "";
+
+if (questionIndex >= questionsObj.length) {
+  endGame();
+  return;
+}
+
+    var h1El=document.createElement("h1");
+    h1El.textContent = questionsObj[questionIndex].questionText
+    mainEl.appendChild(h1El);
+    // we create an element to carry the text of the questions and append it to appear on the screen
+
+    var btnDivEl = document.createElement("div");
+    mainEl.appendChild(btnDivEl);
+
+    var pEl = document.createElement("p");
+    pEl.textContent = rightOrWrong;
+    mainEl.appendChild(pEl);
+
+    btnDivEl.addEventListener("click", function(event){
+      var target=event.target;
+      // console.log("hit")
+
+      if (target.getAttribute("class") !== "btn") return;
+      
+      var clickedQuestionIndex = parseInt(target.getAttribute("data-index"));
+      // parse to change the string into a number 
+      // we're grabbing the index of the possible answers to compare it later
+
+      if (clickedQuestionIndex === questionsObj[questionIndex].correctAnswer){
+        rightOrWrong = "Correct!"
+      } else {
+        time = time - 10;
+        rightOrWrong = "Wrong!"
+      }
+
+
+      questionIndex++;
+      displayQuestions();
+    })
+
+
+    for (var  i = 0; i<questionsObj[questionIndex].possibleAnswers.length; i++){
+      var buttonEl = document.createElement("button");
+      buttonEl.textContent=questionsObj[questionIndex].possibleAnswers[i];
+      buttonEl.setAttribute("class", "btn");
+      buttonEl.setAttribute("data-index", i);
+      btnDivEl.appendChild(buttonEl);
+
+    }
+
+    
+
+
+  
+  }
+
+
+
+
+
+function endGame() {
+  clearInterval(timerInterval);
+  quizDone.hidden=false;
+  console.log("quiz done")
+
+
+  // form goes here
+  // local storage
+
+
+}
+
+
+
--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,196 +1,187 @@
-var startBtn = document.querySelector("#start-button");
-var startPage = document.querySelector(".start-page");
-var questionDisplay = document.querySelector("#question");
-var quizButtonContainer = document.querySelector("#quiz-buttons");
-var finishedP = document.querySelector("#finished");
-var submitBtn = document.querySelector("#submit-button");
-var timer = document.querySelector(".timer");
-var userNotePTag = document.querySelector("#user-note");
-var timeLeft = 20;
-var timeStart;
-var highScores = document.querySelector("#highscores");
+// var startBtn = document.querySelector("#start-button");
+// var startPage = document.querySelector(".start-page");
+// var questionDisplay = document.querySelector("#question");
+// var quizButtonContainer = document.querySelector("#quiz-buttons");
+// var finishedP = document.querySelector("#finished");
+// var submitBtn = document.querySelector("#submit-button");
+// var timer = document.querySelector(".timer");
+// var userNotePTag = document.querySelector("#user-note");
+// var timeLeft = 80;
+// var timeStart;
+// var highScores = document.querySelector("#highscores");
 
-var quizButtons = quizButtonContainer.querySelectorAll("button");
+// var quizButtons = quizButtonContainer.querySelectorAll("button");
 
-var counter = 0;
+// var counter = 0;
 
 
-// var questionsContainer = document.querySelector(".questions-container") 
+// // var questionsContainer = document.querySelector(".questions-container") 
 
-// var showQuestions = document.querySelector(".show-questions");
+// // var showQuestions = document.querySelector(".show-questions");
 
-// when I hit the start quiz button, I want my questions to show 
-// one by one
+// // when I hit the start quiz button, I want my questions to show 
+// // one by one
 
-// when they choose an answer, I want to say wrong or right
+// // when they choose an answer, I want to say wrong or right
 
-// when they finish one question, I want a next button for the next one
+// // when they finish one question, I want a next button for the next one
 
-// 
+// // 
 
-var questions = [
-  {
-  Question: "What is the capital of Greece?", 
-  PossibleAnswers: ["Athens", "Thessaloniki", "Patra"],
-  CorrectAnswer: 0
-  },
+// var questions = [
+//   {
+//   Question: "What is the capital of Greece?", 
+//   PossibleAnswers: ["Athens", "Thessaloniki", "Patra"],
+//   CorrectAnswer: 0
+//   },
 
-  {
-  Question: "What is a neighboring country of Greece?", 
-  PossibleAnswers: ["Spain", "Switzerland", "Turkey"],
-  CorrectAnswer: 2
-  },
+//   {
+//   Question: "What is a neighboring country of Greece?", 
+//   PossibleAnswers: ["Spain", "Switzerland", "Turkey"],
+//   CorrectAnswer: 2
+//   },
 
-  {
-  Question: "What is the biggest port of Greece?", 
-  PossibleAnswers: ["Patra", "Pireaus", "Thessaloniki"],
-  CorrectAnswer: 1
-  },
+//   {
+//   Question: "What is the biggest port of Greece?", 
+//   PossibleAnswers: ["Patra", "Pireaus", "Thessaloniki"],
+//   CorrectAnswer: 1
+//   },
 
-  {
-  Question: "What is a traditional food of Greece?", 
-  PossibleAnswers: ["Souvlaki", "Pasta", "Falafel"],
-  CorrectAnswer: 0
-  },
+//   {
+//   Question: "What is a traditional food of Greece?", 
+//   PossibleAnswers: ["Souvlaki", "Pasta", "Falafel"],
+//   CorrectAnswer: 0
+//   },
   
-  {
-  Question: "Which city is the Acropolis in?", 
-  PossibleAnswers: ["Athens", "Volos", "Kalamata"],
-  CorrectAnswer: 0
-  },
-]
+//   {
+//   Question: "Which city is the Acropolis in?", 
+//   PossibleAnswers: ["Athens", "Volos", "Kalamata"],
+//   CorrectAnswer: 0
+//   },
+// ]
 
-var questionIndex = 0;
-
-
-startBtn.addEventListener("click", startQuiz);
-
-function startQuiz() {
-  //hide start button
-  startBtn.remove();
-  startPage.remove();
-  quizButtonContainer.hidden = false;
-  questionDisplay.hidden = false;
-  displayAnswer();
-
-  quizButtonContainer.style.display = 'block' //setAttribute("stye", "display:block)"
+// var questionIndex = 0;
 
 
-  timeStart = setInterval(function() {
-    timeLeft--
-    timer.textContent = "Time: " + timeLeft
-    if (timeLeft <= 0){
-      endQuiz()
-    }
-  },1000)
+// startBtn.addEventListener("click", startQuiz);
 
-}
+// function startQuiz() {
+//   //hide start button
+//   startBtn.remove();
+//   startPage.remove();
+//   quizButtonContainer.hidden = false;
+//   questionDisplay.hidden = false;
+//   displayAnswer();
 
-
-var displayAnswer = function (){
-  questionDisplay.textContent = questions[questionIndex].Question;
-  for (var i=0; i<quizButtons.length; i++) {
-    quizButtons[i].textContent = questions[questionIndex].PossibleAnswers[i];
-  }
-}
+//   quizButtonContainer.style.display = 'block' //setAttribute("stye", "display:block)"
 
 
-
-
-// function checkAnswer (){
-  
-  // var rightAnswer;
-  // var wrongAnswer;
-
-//   for (var i=0; i < questionDisplay.length; i++){
-//     for (var j = 0 ; j < PossibleAnswers.length; j++) {
-//       if (questions.PossibleAnswers[j] === questions.CorrectAnswer){
-//         print = ("Correct!");
-//         console.log("correct");
-//       } else {
-//         print = ("Wrong!")
-//         console.log("wrong");
-      
-//       }
+//   timeStart = setInterval(function() {
+//     timeLeft--
+//     timer.textContent = "Time: " + timeLeft
+//     if (timeLeft <= 0){
+//       endQuiz()
 //     }
+//   },1000)
+
+// }
+
+
+// var displayAnswer = function (){
+//   questionDisplay.textContent = questions[questionIndex].Question;
+//   for (var i=0; i<quizButtons.length; i++) {
+//     quizButtons[i].textContent = questions[questionIndex].PossibleAnswers[i];
 //   }
 // }
 
-// buttonIndex = 0;
-
-// quizButtons.addEventListener("click", checkAnswer);
 
 
-startBtn.addEventListener("click", startQuiz);
+// startBtn.addEventListener("click", startQuiz);
 
+// var rightOrWrong = [];
 
-for (var i=0; i < quizButtons.length; i++){
-  quizButtons[i].addEventListener("click", function(event){
+// for (var i=0; i < quizButtons.length; i++){
+//   quizButtons[i].addEventListener("click", function(event){
     
-    var targetButtonIndex = parseInt(event.target.dataset.index);
-    
-    if (targetButtonIndex === questions[questionIndex].CorrectAnswer) {
-      console.log('correct');
+//     var targetButtonIndex = parseInt(event.target.dataset.index);
+
+//     if (targetButtonIndex === questions[questionIndex].CorrectAnswer) {
+//       // console.log('correct');
       
-      userNotePTag.textContent = 'Correct!'
-      // quizButtons.style.background = "green";
+//       userNotePTag.textContent = 'Correct!'
+      
+//       // quizButtons.setAttribute("style", "color: green;");
+//     } else {
+//       // console.log('incorrect');
+//       timeLeft = timeLeft - 10;
+//       userNotePTag.textContent = 'Incorrect!'
+//       // quizButtons.setAttribute("style", "color: red;");
 
-    } else {
-      console.log('incorrect');
-      timeLeft = timeLeft - 10;
-      userNotePTag.textContent = 'Incorrect!'
-    }
+//     }
     
-    questionIndex++;
+
+//     questionIndex++;
     
-    if (questionIndex < questions.length){
-      displayAnswer();
-    } else {
-      questionDisplay.hidden=true;
-      userNotePTag.remove();
-      quizButtonContainer.remove();
-      endQuiz();
-    }
-  })
-}
+//     if (questionIndex < questions.length){
+//       displayAnswer();
+//     } else {
+//       questionDisplay.hidden=true;
+//       quizButtonContainer.remove();
+//       endQuiz();
+//     }
+//   })
+// }
 
 
-function endQuiz() {
-  clearInterval(timeStart);
-  console.log('quiz ended')
-  finishedP.hidden = false;
-  submitBtn.hidden = false;
-  highScores.hidden = true;
-  questionDisplay.hidden = true;
-  quizButtonContainer.remove();
-  userNotePTag.remove();
+// function endQuiz() {
+//   clearInterval(timeStart);
+//   console.log('quiz ended')
+//   finishedP.hidden = false;
+//   submitBtn.hidden = false;
+//   highScores.hidden = true;
+//   questionDisplay.hidden = true;
+//   quizButtonContainer.remove();
+
+// }
+
+// function submit(){
+//   highScores.hidden = false;
+//   finishedP.hidden=true;
+//   submitBtn.hidden=true;
+//   userNotePTag.remove();
 
 
-}
+// }
 
-function submit(){
-  highScores.hidden = false;
-  finishedP.hidden=true;
-  submitBtn.hidden=true;
-  userNotePTag.remove();
+// submitBtn.addEventListener("click", submit);
+
+// // Show results and submit buttons
 
 
-}
+// // connect correct answer
+// // print wrong or right
+// // highscores
+// // subtract time from clock for wrong answer
 
-submitBtn.addEventListener("click", submit);
-
-// Show results and submit buttons
-
-
-// connect correct answer
-// print wrong or right
-// highscores
-// subtract time from clock for wrong answer
-
-// add event listener when hit the correct answer text to turn green 
+// // add event listener when hit the correct answer text to turn green 
 
 
 
-// local storage
+// // local storage
 
-// submit even prevent default
+// // submit even prevent default
+
+
+
+// // set time out for the answer in question after the answer, to delay when the next question shows
+// // put the display question in the set time out
+// // store the results in an array of objects, key initials, value their name, score and value 
+// // loop over the array and display in the highscores 
+
+
+
+
+
+
+
+
